9a1ae85d9342939e70608d5f8a67823101c91d9a
Alexander Hadion <gadion_alex@ukr.net>
2025-05-20 00:27:17 +0300

    add task 1 2 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,15 @@
-function getElementWidth(content, padding, border) {
-  let numContent = content.replace(',', '.');
-  numContent = parseFloat(numContent);
-  let numPadding = padding.replace(',', '.');
-  numPadding = parseFloat(numPadding);
-  let numBorder = border.replace(',', '.');
-  numBorder = parseFloat(numBorder);
-  return numContent + 2 * numPadding + 2 * numBorder;
+function filterArray(numbers, value) {
+  let numbered = [];
+  for (const elem of numbers) {
+    if (elem > value) {
+      numbered.push(elem);
+    }
+  }
+  return numbered;
 }
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8,5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
